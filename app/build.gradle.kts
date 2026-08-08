f5c11af031f11f25b85cffd0d85e6154fa4967ca
Amal <muhammad.ichsanul19@gmail.com>
2026-08-08 07:36:45 +0700

    chore: bump version to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.sans.finance"
         minSdk = 36
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
